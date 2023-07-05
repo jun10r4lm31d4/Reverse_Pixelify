83b30fffb9e3801cae7ea7e54b3f4da7dd1ca78e
Júnior Andrade de Almeida <42343358+juniiimoh@users.noreply.github.com>
2023-07-04 22:41:23 -0300

    Update HookEntry.kt

--- app/src/main/java/com/uragiristereo/reversepixelify/HookEntry.kt
+++ app/src/main/java/com/uragiristereo/reversepixelify/HookEntry.kt
@@ -13,85 +13,26 @@ class HookEntry : IYukiHookXposedInit {
     companion object {
         const val TAG = "ReversePixelify"
 
-        val attestationHooksClasses = listOf(
-            AttestationHooksClass(
-                rom = "PixelExperience",
-                className = "com.android.internal.util.custom.AttestationHooks",
-                methodName = "initApplicationBeforeOnCreate",
-            ),
-            AttestationHooksClass(
-                rom = "EvolutionX",
-                className = "com.android.internal.util.evolution.AttestationHooks",
-                methodName = "initApplicationBeforeOnCreate",
-            ),
-            AttestationHooksClass(
-                rom = "CrDroidAndroid",
-                className = "com.android.internal.util.crdroid.AttestationHooks",
-                methodName = "initApplicationBeforeOnCreate",
-            ),
-            AttestationHooksClass(
-                rom = "Xtended",
-                className = "com.android.internal.util.xtended.AttestationHooks",
-                methodName = "initApplicationBeforeOnCreate",
-            ),
-        )
-
         val pixelPropsUtilClasses = listOf(
             PixelPropsUtilClass(
                 rom = "ProjectElixir",
                 className = "com.android.internal.util.PixelPropsUtils",
                 methodName = "setProps",
             ),
-            PixelPropsUtilClass(
-                rom = "PixelExperience",
-                className = "com.android.internal.util.custom.PixelPropsUtils",
-                methodName = "setProps",
-            ),
-            PixelPropsUtilClass(
-                rom = "EvolutionX",
-                className = "com.android.internal.util.evolution.PixelPropsUtils",
-                methodName = "setProps",
-            ),
-            PixelPropsUtilClass(
-                rom = "CrDroidAndroid",
-                className = "com.android.internal.util.crdroid.PixelPropsUtils",
-                methodName = "setProps",
-            ),
-            PixelPropsUtilClass(
-                rom = "Xtended",
-                className = "com.android.internal.util.xtended.PixelPropsUtils",
-                methodName = "setProps",
-            ),
         )
     }
 
-    private val attestationHooksClass = getAttestationHooksClass()
     private val pixelPropUtilClass = getPixelPropsUtilClass()
 
     override fun onInit() = configs {
         isDebug = BuildConfig.DEBUG
 
-        if (attestationHooksClass == null || pixelPropUtilClass == null) {
+        if (pixelPropUtilClass == null) {
             log("Your ROM doesn't have internal spoofing or unsupported.")
         }
     }
 
     override fun onHook() = encase {
-        if (attestationHooksClass != null) {
-            loadApp {
-                attestationHooksClass.className.hook {
-                    injectMember {
-                        method { name = attestationHooksClass.methodName }
-
-                        beforeHook {
-                            log("Revert spoofing for $packageName")
-
-                            resultNull()
-                        }
-                    }
-                }
-            }
-        }
         if (pixelPropUtilClass != null) {
             loadApp {
                 pixelPropUtilClass.className.hook {
@@ -107,18 +48,6 @@ class HookEntry : IYukiHookXposedInit {
                 }
             }
         }
-    }
-
-    private fun getAttestationHooksClass(): AttestationHooksClass? {
-        for (clazz in attestationHooksClasses) {
-            try {
-                clazz.className.toClass()
-
-                return clazz
-            } catch (_: NoClassDefFoundError) { }
-        }
-
-        return null
     }
 
     private fun getPixelPropsUtilClass(): PixelPropsUtilClass? {
